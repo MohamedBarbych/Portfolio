3e30da200be8396591190a34f4b5697ee96761c4
MohamedBarbych <146338565+MohamedBarbych@users.noreply.github.com>
2024-10-07 21:47:13 +0100

    Update techno.jsx

--- src/Components/Techno.jsx
+++ src/Components/Techno.jsx
@@ -1,100 +1,161 @@
 import { RiReactjsLine } from "react-icons/ri";
-import { SiMongodb } from "react-icons/si";
+import {
+  SiMongodb,
+  SiSymfony,
+  SiAngular,
+  SiTailwindcss,
+  SiNodedotjs,
+  SiSpringboot,
+  SiFlask,
+  SiPython,
+} from "react-icons/si";
 import { BiLogoPostgresql } from "react-icons/bi";
-import { SiSymfony } from "react-icons/si";
-import { SiAngular } from "react-icons/si";
-import { SiTailwindcss } from "react-icons/si";
-import { SiNodedotjs } from "react-icons/si";
-import UnityLogo from '../assets/unity.png';
-import { motion } from "framer-motion"
-
+import UnityLogo from "../assets/unity.png";
+import { motion } from "framer-motion";
 
 const iconVariants = (duration) => ({
-    initial: { y: -10 },
-    animate: { 
-        y: [10, -10],
-        transition: {
-            duration: duration,
-            ease: "linear",
-            repeat: Infinity,
-            repeatType: "reverse",
-        },
-     },
-})
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
 const Techno = () => {
   return (
     <div className="border-b border-neutral-800 pb-24 ">
-    <motion.h2
-    whileInView={{ opacity: 1, y: 0 }}
-    initial = {{ opacity: 0, y: -100 }}
-    transition={{ duration: 1.5 }}
-     className="my-20 text-center text-4xl">Technologies</motion.h2>
-        <motion.div 
-        whileInView={{opacity: 1, x: 0}}
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h2>
+
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
         initial={{ opacity: 0, x: -100 }}
-        transition={{ duration: 1.5 }} 
-        className="flex flex-wrap items-center justify-center gap-4">
-            <motion.div 
-            variants={iconVariants(2.5)}
-            initial="initial"
-            animate="animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-                 <img src={UnityLogo} alt="Unity Logo" className="w-16 h-16" />
-            </motion.div>
-            <motion.div 
-            variants={iconVariants(3)}
-            initial="initial"
-            animate="animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-                <RiReactjsLine className="text-7xl text-cyan-400 "/>
-            </motion.div>
-            <motion.div
-            variants={iconVariants(5)}
-            initial="initial"
-            animate="animate"
-             className="rounded-2xl border-4 border-neutral-800 p-4">
-                <SiSymfony className="text-7xl"/>
-            </motion.div>
-            <motion.div
-            variants={iconVariants(7)}
-            initial="initial"
-            animate="animate"
-             className="rounded-2xl border-4 border-neutral-800 p-4">
-                <SiAngular className="text-7xl text-red-500 "/>
-            </motion.div>
-            <motion.div 
-            variants={iconVariants(2.5)}
-            initial="initial"
-            animate="animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-                <SiMongodb className="text-7xl text-green-500 "/>
-            </motion.div>
-            <motion.div 
-            variants={iconVariants(4)}
-            initial="initial"
-            animate="animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-                <SiTailwindcss className="text-7xl text-cyan-600 "/>
-            </motion.div>
-            <motion.div 
-            variants={iconVariants(3.5)}
-            initial="initial"
-            animate="animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-                <SiNodedotjs className="text-7xl text-green-500 "/>
-            </motion.div>
-            <motion.div 
-            variants={iconVariants(7)}
-            initial="initial"
-            animate="animate"
-            className="rounded-2xl border-4 border-neutral-800 p-4">
-                <BiLogoPostgresql className="text-7xl text-sky-700 "/>
-            </motion.div>
-
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        {/* Unity Logo */}
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <img src={UnityLogo} alt="Unity Logo" className="w-16 h-16" />
         </motion.div>
-    </div>
-  )
-}
 
-export default Techno
+        {/* React */}
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-7xl text-cyan-400 " />
+        </motion.div>
+
+        {/* Symfony */}
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiSymfony className="text-7xl" />
+        </motion.div>
+
+        {/* Angular */}
+        <motion.div
+          variants={iconVariants(7)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiAngular className="text-7xl text-red-500 " />
+        </motion.div>
+
+        {/* MongoDB */}
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiMongodb className="text-7xl text-green-500 " />
+        </motion.div>
+
+        {/* Tailwind CSS */}
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiTailwindcss className="text-7xl text-cyan-600 " />
+        </motion.div>
+
+        {/* Node.js */}
+        <motion.div
+          variants={iconVariants(3.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiNodedotjs className="text-7xl text-green-500 " />
+        </motion.div>
+
+        {/* PostgreSQL */}
+        <motion.div
+          variants={iconVariants(7)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <BiLogoPostgresql className="text-7xl text-sky-700 " />
+        </motion.div>
+
+        {/* Spring Boot */}
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiSpringboot className="text-7xl text-green-600" />
+        </motion.div>
+
+        {/* Python */}
+        <motion.div
+          variants={iconVariants(3.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiPython className="text-7xl text-yellow-400" />
+        </motion.div>
+
+        {/* Flask */}
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiFlask className="text-7xl text-gray-600" />
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Techno;
