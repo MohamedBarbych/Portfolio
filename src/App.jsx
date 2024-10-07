503accae83afd4bfb218fdd937f7e9ec2b98cc82
MohamedBarbych <146338565+MohamedBarbych@users.noreply.github.com>
2024-10-07 22:13:19 +0100

    last fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,28 +10,13 @@ import ParticlesComponent from "./ParticlesComponent";
 import NavSideBar from "./Components/SideBar";
 import ChatbotScript from "./Components/ChatbotScript";
 import LatestActivities from "./Components/LatestActivities";
-import LightningAnimation from "./Components/LightningAnimation"; // Import the animation component
 
 import { LATEST_ACTIVITIES } from "./assets/constants/index";
 
 const App = () => {
-  const [showLightning, setShowLightning] = useState(true);
-
-  // Hide the animation after 5 seconds
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowLightning(false);
-    }, 5000); // 5 seconds duration for the lightning effect
-
-    return () => clearTimeout(timer); // Cleanup the timer
-  }, []);
-
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 relative">
       <ParticlesComponent />
-
-      {/* Conditionally render the lightning animation */}
-      {showLightning && <LightningAnimation />}
 
       <div className="fixed top-0 -z-10 h-full w-full">
         <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
