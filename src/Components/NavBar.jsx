3cc89d6a574b24fce855d3a7bce4bb95bc443d16
MohamedBarbych <146338565+MohamedBarbych@users.noreply.github.com>
2024-10-07 22:18:43 +0100

    Update Navbar.jsx adding facebook icon

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,8 +1,11 @@
 import logo from "../assets/C.png";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa";
+import {
+  FaLinkedin,
+  FaGithub,
+  FaSquareXTwitter,
+  FaInstagram,
+  FaFacebook,
+} from "react-icons/fa";
 
 const NavBar = () => {
   return (
@@ -11,16 +14,28 @@ const NavBar = () => {
         <img className="mx-2 w-20" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <a href="https://www.linkedin.com/in/mohamed-barbych-42217b276?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2F74SOQ%2FnTOmk3e2aQN3NSw%3D%3D">
+        <a
+          href="https://www.linkedin.com/in/mohamed-barbych-42217b276?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2F74SOQ%2FnTOmk3e2aQN3NSw%3D%3D"
+          aria-label="LinkedIn"
+        >
           <FaLinkedin />
         </a>
-        <a href="https://github.com/MohamedBarbych">
+        <a href="https://github.com/MohamedBarbych" aria-label="GitHub">
           <FaGithub />
         </a>
-        <a href="https://x.com/MBarbych50505">
+        <a href="https://x.com/MBarbych50505" aria-label="X (Twitter)">
           <FaSquareXTwitter />
         </a>
-        <FaInstagram />
+        <a href="https://www.instagram.com" aria-label="Instagram">
+          <FaInstagram />
+        </a>
+        {/* Add Facebook link */}
+        <a
+          href="https://www.facebook.com/Mohamed.Barbych1?mibextid=ZbWKwL"
+          aria-label="Facebook"
+        >
+          <FaFacebook />
+        </a>
       </div>
     </nav>
   );
