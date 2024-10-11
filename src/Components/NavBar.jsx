9bb9768d59240047b9ea18d3c325c31cbe6f266b
MohamedBarbych <146338565+MohamedBarbych@users.noreply.github.com>
2024-10-11 19:33:54 +0100

    updating the navbar  : opening in a new tab

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,10 @@
 import logo from "../assets/C.png";
 import { FaLinkedin, FaGithub, FaInstagram, FaFacebook } from "react-icons/fa";
-
 import { FaSquareXTwitter } from "react-icons/fa6";
 
 const NavBar = () => {
   return (
-    <nav className=" mb-20 flex items-center justify-between py-6">
+    <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         <img className="mx-2 w-20" src={logo} alt="logo" />
       </div>
@@ -13,22 +12,40 @@ const NavBar = () => {
         <a
           href="https://www.linkedin.com/in/mohamed-barbych-42217b276?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2F74SOQ%2FnTOmk3e2aQN3NSw%3D%3D"
           aria-label="LinkedIn"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <FaLinkedin />
         </a>
-        <a href="https://github.com/MohamedBarbych" aria-label="GitHub">
+        <a
+          href="https://github.com/MohamedBarbych"
+          aria-label="GitHub"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FaGithub />
         </a>
-        <a href="https://x.com/MBarbych50505" aria-label="X (Twitter)">
+        <a
+          href="https://x.com/MBarbych50505"
+          aria-label="X (Twitter)"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FaSquareXTwitter />
         </a>
-        <a href="https://www.instagram.com" aria-label="Instagram">
+        <a
+          href="https://www.instagram.com"
+          aria-label="Instagram"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FaInstagram />
         </a>
-        {/* Add Facebook link */}
         <a
           href="https://www.facebook.com/Mohamed.Barbych1?mibextid=ZbWKwL"
           aria-label="Facebook"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <FaFacebook />
         </a>
