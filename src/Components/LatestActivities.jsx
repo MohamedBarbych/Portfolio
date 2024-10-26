5943a1f6186bb1ea1512896dc33ad30e653ed54a
MohamedBarbych <MohamedBarbych@users.noreply.github.com>
2024-10-26 18:59:12 +0100

    Fixing errors

--- src/Components/LatestActivities.jsx
+++ src/Components/LatestActivities.jsx
@@ -63,7 +63,7 @@ const LatestActivities = ({ activities }) => {
               src={activity.image}
               alt={activity.title}
               className="w-full h-full object-cover cursor-pointer"
-              onClick={() => handleImageClick(activity.image)}
+              onClick={() => handleImageClick(activity.image)}//
             />
             <div className="absolute bottom-0 left-0 right-0 p-4 bg-black bg-opacity-50 text-white">
               <h3 className="text-lg font-semibold">{activity.title}</h3>
