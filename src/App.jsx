109670714c0dd3d1fc3150a3f6d6d2ae6560b91a
MohamedBarbych <MohamedBarbych@users.noreply.github.com>
2024-10-26 18:49:52 +0100

    Updating app.jsx for Certification integration

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ const App = () => {
             <ParticlesComponent />
 
             <div className="fixed top-0 -z-10 h-full w-full">
-                <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+                <div className="absolute inset-0 -z-10 h-full w-full bg-gradient-to-b from-black to-[#6300e4]"></div>
             </div>
 
             <div className="container mx-auto px-8 relative">
@@ -53,6 +53,7 @@ const App = () => {
                     <LanguageSwitcher />
                 </div>
                 <NavSideBar />
+                
                 <section id="hero">
                     <Hero />
                 </section>
@@ -64,12 +65,15 @@ const App = () => {
                 <section id="about">
                     <About />
                 </section>
+                
                 <section id="techno">
                     <Techno />
                 </section>
-                <section id="techno">
+                
+                <section id="certifications">
                     <Certification />
                 </section>
+                
                 <section id="experience">
                     <Experience />
                 </section>
@@ -77,6 +81,7 @@ const App = () => {
                 <section id="projects">
                     <Projects />
                 </section>
+                
                 <section id="contact">
                     <Contact />
                 </section>
