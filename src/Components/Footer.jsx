89ed650a9ad923a1e6d5a932ea112a95ab323309
MohamedBarbych <MohamedBarbych@users.noreply.github.com>
2024-10-31 19:35:03 +0100

    Updating the footer still

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-    // ---------------- Get the current year ------------------
+  // ---------------- Get the current year ------------------
   const year = new Date().getFullYear(); 
 
   return (
-    <footer className="bg-neutral-900 text-neutral-300 py-4 text-center">
+    <footer className="bg-transparent text-neutral-300 py-4 text-center">
       <p className="text-sm">
         &copy; {year} Mohamed BARBYCH. All rights reserved.
       </p>
