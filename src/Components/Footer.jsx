952e2d51ee6a6c1a8f0958bd439f0a0aa20fc07b
MohamedBarbych <MohamedBarbych@users.noreply.github.com>
2024-11-10 15:54:50 +0100

    updating footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  // -------------- Get the current year ----------------
+  // -------------- Get the_current year ----------------
   const year = new Date().getFullYear(); 
 
   return (
